a96bb6e5a83729dcdb941c4d2703f1ee4ac9ac8a
garrettgarrett <skfelix1@gmail.com>
2025-01-13 12:25:29 -0800

    never cache build

--- app/api/build/route.ts
+++ app/api/build/route.ts
@@ -1,12 +1,18 @@
 import { extractJsonFromHtml } from "@/app/utils/userDataParser";
 
 export async function GET() {
-  const response = await fetch("https://skool.com");
+  const response = await fetch("https://skool.com", {
+    cache: 'no-store',
+    headers: {
+      'Cache-Control': 'no-cache',
+      'Pragma': 'no-cache'
+    }
+  });
   const html = await response.text();
   const jsonData = extractJsonFromHtml(html);
 
   console.log({ jsonData });
 
-  const buildId = jsonData.buildId; // Assuming buildId is a direct property of the extracted JSON
-  return Response.json({ buildId }); // Return the buildId
+  const buildId = jsonData.buildId;
+  return Response.json({ buildId });
 }
